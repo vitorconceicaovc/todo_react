8d3ed126a05c204d628485584e6bf398b6ec6e11
Vítor Conceição <vitoralmeidavc@outlook.pt>
2022-02-13 17:34:35 +0000

    v1

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+import './Button.css';
+
+const Button = ({children, onClick}) => {
+    return (
+        <button onClick={onClick} className="button">
+            {children}
+        </button>
+    );
+};
+ 
+export default Button;
